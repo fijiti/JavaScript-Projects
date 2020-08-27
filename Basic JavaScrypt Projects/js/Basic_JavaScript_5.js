0ea9ad734b0949347c758cbec2d1bfbabdd4199f
fijiti <fijiti@users.noreply.github.com>
2020-08-27 14:59:19 -0700

    Correct Basic_JavaScript_5 Add && < ! operators

--- Basic JavaScrypt Projects/js/Basic_JavaScript_5.js
+++ Basic JavaScrypt Projects/js/Basic_JavaScript_5.js
@@ -9,24 +9,28 @@ document.write(`<h1>JavaScript and Boolean Logic</h1>
 
 // Using coercion in JavaScript
 function coercion_Function(){
-    document.getElementById("test1").innerHTML = ("Test Coercion : '10' + 5 = " + ("10" + 5));
+    document.getElementById("test16").innerHTML = ("Test Coercion : '10' + 5 = " + ("10" + 5));
 }
 
 // Using my_Function to show different boolean tests. 
 function boolean_Function() {
     
-    document.getElementById("test2").innerHTML = ("Test 5 > 2 : " + (5 > 2));
-    document.getElementById("test3").innerHTML = ("Test 8 > 12 : " + (8 > 12));
-    document.getElementById("test4").innerHTML = ("Test 45 == 45 : " + (45 == 45));
-    document.getElementById("test5").innerHTML = ("Test 345 == 355 : " + (345 == 455));
-    document.getElementById("test6").innerHTML = ("Test 55 === 55 : " + (55 === 55));
-    document.getElementById("test7").innerHTML = ("Test 55 === '555' : " + (55 === "555"));
-    document.getElementById("test8").innerHTML = ("Test 55 === '55' : " + (55 === "55"));
-    document.getElementById("test9").innerHTML = ("Test 55 === 5555 : " + (55 === 555));
-    document.getElementById("test10").innerHTML = (" Test 44 > 4 || 44 > 55 : " + (44 > 4 || 44 > 55));
-    document.getElementById("test11").innerHTML = (" Test 2 > 4 || 44 > 55 : " + (2 > 4 || 44 > 55));
-    document.getElementById("test12").innerHTML = (" Test 44 > 4 && 44 > 3 : " + (44 > 4 || 44 > 3));
-    document.getElementById("test13").innerHTML = (" Test 44 > 4 && 44 > 55 : " + (44 > 4 || 44 > 55));
+    document.getElementById("test1").innerHTML = ("Test 44 != 44 : " + (44 != 44));
+    document.getElementById("test2").innerHTML = ("Test 55 != 33 : " + (55 != 33));
+    document.getElementById("test3").innerHTML = ("Test !false :" + !false);
+    document.getElementById("test4").innerHTML = ("Test 5 > 2 : " + (5 > 2));
+    document.getElementById("test5").innerHTML = ("Test 12 < 8 : " + (12 < 8));
+    document.getElementById("test6").innerHTML = ("Test 45 == 45 : " + (45 == 45));
+    document.getElementById("test7").innerHTML = ("Test 345 == 355 : " + (345 == 455));
+    document.getElementById("test8").innerHTML = ("Test 55 === 55 : " + (55 === 55));
+    document.getElementById("test9").innerHTML = ("Test 55 === '555' : " + (55 === "555"));
+    document.getElementById("test10").innerHTML = ("Test 55 === '55' : " + (55 === "55"));
+    document.getElementById("test11").innerHTML = ("Test 55 === 5555 : " + (55 === 555));
+    document.getElementById("test12").innerHTML = ("Test 44 > 4 || 55 < 44 : " + (44 > 4 || 55 < 44));
+    document.getElementById("test13").innerHTML = ("Test 2 > 4 || 77 < 55 : " + (2 > 4 || 77 < 55));
+    document.getElementById("test14").innerHTML = ("Test 44 > 4 && 3 < 44 : " + (44 > 4 && 3 < 44));
+    document.getElementById("test15").innerHTML = ("Test 44 > 4 && 44 > 55 : " + (44 > 4 && 44 > 55));
+    
 }
 
 
